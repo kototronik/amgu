908eab377215ff242ca2affd016c6f42f57df776
kototronik <kototronik@gmail.com>
2026-02-11 23:45:22 +0900

    визуальные украшения

--- js/renderer.js
+++ js/renderer.js
@@ -68,7 +68,6 @@ export function render(shouldScroll = false) {
 
     DOM.weekStatus.innerHTML = `
         <div style="display: flex; align-items: center; gap: 6px;">
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="18px" fill="#58a6ff"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm360-80h100v-480H520v480Zm-180 0h100v-480H340v480Zm-180 0h100v-480H160v480Zm540 0h100v-480H700v480Z"/></svg>
         <span>${parity}-я неделя</span>
         </div>
     `;
